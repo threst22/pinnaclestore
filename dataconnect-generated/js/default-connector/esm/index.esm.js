a5cecb8fa5c38bda75acbe58a8fade197a35646c
threst22 <harry.timosa@gmail.com>
2025-06-17 02:44:07 +0800

    Add Firebase setup

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'pinnacle-rewards-store',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'pinnacle-rewards-store',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
